c1599a08ebad21189e9f27032ad440850a9b785e
AndrewAnduWXM <andutzzu@gmail.com>
2026-08-14 08:43:21 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'abus-productie-v1';
+const APP_SHELL = [
+  './AbusProductieMobil.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Network-first pentru date live (Google Apps Script), cache-first pentru shell-ul aplicației
+self.addEventListener('fetch', (event) => {
+  const url = event.request.url;
+  if (url.includes('script.google.com')) {
+    // Nu interceptăm cererile către backend - mereu direct din rețea
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((response) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      }).catch(() => cached);
+    })
+  );
+});
